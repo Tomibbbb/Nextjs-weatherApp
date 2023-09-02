70770a41e3ef3151e1274d5e5da1515f54d41da3
Tomibbbb <victoriabanjo16@gmail.com>
2023-09-02 13:24:11 +0100

    First commit

---
+++ component/Weather.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Weather = ({data}) => {
+console.log(data)
+  return (
+    <div>
+        <div>
+            <div>
+                <Image alt='/' /> 
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Weather
